7780dab1d46787664245327c16e6cff6599d6e0d
saied <saiedrahimigolafshani@gmail.com>
2023-12-03 18:16:38 +0330

    Config_ErrorHandler

--- Main.js
+++ Main.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const dataBase_Config = require("./Src/Config/Mongoose.Config");
+const notFoundHandler = require("./Src/Common/Exception/notFound.Handler");
 
 dotenv.config();
 async function main(){
@@ -8,7 +10,9 @@ async function main(){
     const baseUrl = process.env.BASEURL;
     app.use(express.json());
     app.use(express.urlencoded({extended: true}));
-
+    dataBase_Config();
+    notFoundHandler(app);
+    allExceptionHandler(app)
 
     app.listen(port, () => {
         console.log(`Server: ${baseUrl}:${port}`);
---
+++ Src/Common/Exception/All_Exception.Handler.js
@@ -0,0 +1,9 @@
+function allExceptionHandler(app){
+    let status = err?.status ?? err?.statusCode ?? err?.code;
+    if(!status || isNaN(+status) || status > 511 || status < 200) status = 500;
+    res.status(status).json({
+        message: err?.message ?? err?.stack ?? "InternalServerError"
+    })
+}
+
+module.exports = allExceptionHandler
